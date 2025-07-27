1591b3f0e87f5f2e03d9d0fd6a2af353a46c1317
Владислав Соголаев <yaroslavsogolaev@yandex.ru>
2025-07-27 21:56:29 +0300

    Удалён проблемный файл upload.j23

--- src/app/api/videos/route.js
+++ src/app/api/videos/route.js
@@ -1,40 +1,35 @@
+export const runtime = 'nodejs'; // 👈 ОБЯЗАТЕЛЬНО
+
 import { NextResponse } from 'next/server';
 import { prisma } from '#lib/prisma';
 import { writeFile } from 'fs/promises';
 import path from 'path';
-
-export async function GET() {
-  try {
-    const reels = await prisma.reel.findMany({
-      include: { likes: true }
-    });
-    return NextResponse.json(reels);
-  } catch (error) {
-    console.error('Ошибка при получении рилсов:', error);
-    return NextResponse.json({ error: 'Ошибка сервера' }, { status: 500 });
-  }
-}
+import fs from 'fs';
 
 export async function POST(req) {
   try {
     const formData = await req.formData();
     const file = formData.get('video');
 
-    if (!file) {
+    if (!file || typeof file.name !== 'string') {
       return NextResponse.json({ error: 'Файл не получен' }, { status: 400 });
     }
 
     const bytes = await file.arrayBuffer();
     const buffer = Buffer.from(bytes);
-    const filePath = path.join(process.cwd(), 'public', 'uploads', file.name);
+
+    const uploadsDir = path.join(process.cwd(), 'public', 'uploads');
+    if (!fs.existsSync(uploadsDir)) {
+      fs.mkdirSync(uploadsDir, { recursive: true });
+    }
+
+    const filePath = path.join(uploadsDir, file.name);
     await writeFile(filePath, buffer);
 
     const newReel = await prisma.reel.create({
-      
       data: {
         title: file.name,
         videoURL: `/uploads/${file.name}`,
-        
       },
     });
 
