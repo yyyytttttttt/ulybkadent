2ffa15e6cca2c54465a96f3c1ad0b74015eaba6a
Владислав Соголаев <yaroslavsogolaev@yandex.ru>
2025-07-27 21:41:55 +0300

    Удалён проблемный файл upload.j22

--- src/app/models/video.js
+++
@@ -1,9 +0,0 @@
-import { DataTypes } from "sequelize";
-import { sequelize } from '#lib/prisma';
-
-export const reel =sequelize.define('Reel',{
-    id:{type:DataTypes.INTEGER,primaryKey:true,autoIncrement:true},
-    title:{type:DataTypes.STRING,allowNull:false},
-    videoURL:{type:DataTypes.STRING,allowNull:false}
-
-})
